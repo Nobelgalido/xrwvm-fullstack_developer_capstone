0584cc5909e8ea1e84f4b813301ea9a0dee524a6
Nobelgalido <galidoalfred@gmail.com>
2025-12-31 07:36:01 -0500

    Remove unused imports and fix linting errors

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,11 +1,6 @@
 # Uncomment the required imports before adding the code
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -133,7 +128,7 @@ def add_review(request):
     if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
-            response = post_review(data)
+            post_review(data)
             return JsonResponse({"status": 200})
         except BaseException:
             return JsonResponse(
